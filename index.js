6b3ded23e4c74566df75fbfb580398604682d523
!  vailes <pashasidlo@gmail.com>
2022-09-30 14:41:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,8 @@ const manager = new ShardingManager('./src/bot.js', {
     totalShards: "auto",
 })
 
-const webhook = new WebhookClient({ url: `https://discord.com/api/webhooks/1024640672717869088/11CPHkgki1y_0ZV6Te-RFKZu9tWYWQbGo1uLAVckqvBKwpV9F8JuP-TLhdplPlxOIX-2` })
-
 manager.on("shardCreate", (shard) => {
     console.log(`[SHARD #${shard.id}]: `.green.bold + `Launched shard `.blue.bold + `${shard.id}`.yellow.bold);
-    webhook.send({embeds: ``})
 });
 
 manager.spawn();
